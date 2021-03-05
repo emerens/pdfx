933889b80277bd80abd4c613e1affcb361e5e037
DaikiMorita <d.morita@outlook.com>
2021-03-05 18:44:50 +0900

    findallした結果を標準出力

--- pdfx/extractor.py
+++ pdfx/extractor.py
@@ -26,6 +26,8 @@ URL_REGEX = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|
 def extract_urls(text):
     matches = re.findall(URL_REGEX, text, re.IGNORECASE)
 
+    print(matches)
+
     if len(matches) == 0:
         return set(matches)
     else:
