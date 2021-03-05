c52f463f6f0575e71f9570c19ce0118d1ef4d97c
DaikiMorita <d.morita@outlook.com>
2021-03-06 00:46:32 +0900

    extract_urlsメソッドを変更

--- pdfx/extractor.py
+++ pdfx/extractor.py
@@ -6,15 +6,21 @@ from __future__ import (absolute_import, division, print_function,
 import re
 
 # ツール用に改変
-URL_REGEX = r"(https)([\-:_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+$,%#]+\n)"
+URL_REGEX1 = r"(https)([\-:_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+$,%#]+\n)"
+URL_REGEX2 = r"(https)([\-:_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+$,%#]+\n)([\-:_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+$,%#]+\n)"
+URL_REGEX3 = r"(https)([\-:_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+$,%#]+\n)([\-:_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+$,%#]+\n)([\-:_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+$,%#]+\n)"
+
+URL_REGEX_list = [URL_REGEX1, URL_REGEX2, URL_REGEX3]
 
 
 def extract_urls(text):
     """
     ツール用に改変
     """
-    url_list = re.findall(URL_REGEX, text)
-    url_list = [''.join(url).replace('\n', '') for url in url_list]
+    url_list = []
+    for URL_REGEX in URL_REGEX_list:
+        urls = re.findall(URL_REGEX, text)
+        url_list += [''.join(url).replace('\n', '') for url in urls]
     return set(url_list)
 
 
@@ -22,8 +28,10 @@ def extract_arxiv(text):
     """
     ツール用に改変
     """
-    url_list = re.findall(URL_REGEX, text)
-    url_list = [''.join(url).replace('\n', '') for url in url_list]
+    url_list = []
+    for URL_REGEX in URL_REGEX_list:
+        urls = re.findall(URL_REGEX, text)
+        url_list += [''.join(url).replace('\n', '') for url in urls]
     return set(url_list)
 
 
@@ -31,8 +39,10 @@ def extract_doi(text):
     """
     ツール用に改変
     """
-    url_list = re.findall(URL_REGEX, text)
-    url_list = [''.join(url).replace('\n', '') for url in url_list]
+    url_list = []
+    for URL_REGEX in URL_REGEX_list:
+        urls = re.findall(URL_REGEX, text)
+        url_list += [''.join(url).replace('\n', '') for url in urls]
     return set(url_list)
 
 
